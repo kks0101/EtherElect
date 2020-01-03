6517e0bfab83c766d9c7eb11926098dd24a27fc9
Krishna Kant Singh <kks1102000@gmail.com>
2020-01-03 23:29:12 +0530

    modified contract and created front end

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,9 +2,20 @@ pragma solidity ^0.5;
 
 contract Election{
 	
-	string public candidate;
-
+	struct Candidate{
+		uint id;
+		string name;
+		uint voteCount;
+	}
+	mapping(uint=> Candidate) public candidates;
+	uint public candidatesCount;
 	constructor() public{
-		candidate = "Candidate 1";
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	function addCandidate(string memory _name ) private{
+		candidatesCount ++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
 }
--- src/js/app.js
+++ src/js/app.js
@@ -3,62 +3,83 @@ App = {
   contracts: {},
 
   init: async function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
-
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-
-        petsRow.append(petTemplate.html());
-      }
-    });
-
     return await App.initWeb3();
   },
 
   initWeb3: async function() {
-    /*
-     * Replace me...
-     */
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      try {
+      // Request account access
+        await window.ethereum.enable();
+      } catch (error) {
+      // User denied account access...
+        console.error("User denied account access")
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider;
+    }
+    // If no injected web3 instance is detected, fall back to Ganache
+    else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3 = new Web3(App.web3Provider);
 
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    $.getJSON("Election.json", function(election){
+      App.contracts.Election = TruffleContract(election);
+      App.contracts.Election.setProvider(App.web3Provider);
 
-    return App.bindEvents();
+      return App.render();
+    });
   },
 
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
-  },
+  render: function(){
+    var electionInstance;
+    var loader = $("#loader");
+    var content = $("#content");
 
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
-  },
+    loader.show();
+    content.hide();
 
-  handleAdopt: function(event) {
-    event.preventDefault();
+    web3.eth.getCoinbase(function(err, account){
+      if(err === null){
+        App.account = account;
+        $("#accountAddress").html("Your account: " + account);
+      }
+    });
 
-    var petId = parseInt($(event.target).data('id'));
+    App.contracts.Election.deployed().then(function(instance){
+      electionInstance = instance;
+      return electionInstance.candidatesCount();
+    }).then(function(candidatesCount){
+      var candidatesResults = $("#candidatesResults");
+      candidatesResults.empty();
+      for(var i = 1; i<= candidatesCount;i++){
+        electionInstance.candidates(i).then(function(candidate){
+          var id = candidate[0];
+          var name = candidate[1];
+          var voteCount = candidate[2];
 
-    /*
-     * Replace me...
-     */
+
+          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td>";
+          candidatesResults.append(candidateTemplate);
+        });
+      }
+      
+      loader.hide();
+      content.show();
+
+    }).catch(function(error){
+      console.warn(error);
+    })
   }
-
 };
 
 $(function() {
